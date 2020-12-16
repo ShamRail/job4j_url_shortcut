5414494ea027b61aa3aa7ea1829dd83758afb73e
Rail <rail1999g102r@mail.ru>
2020-12-16 18:33:47 +0500

    Init commit

---
+++ src/test/java/ru/job4j/shortcut/ShortcutApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.shortcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortcutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
